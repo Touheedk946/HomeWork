439e29b54fc77275d6116d7b6f73ec51b385fc12
Touheed khan <touheedk946@gmail.com>
2024-09-04 15:35:46 +0530

    Added 4 Modules with Java classes
    Oops Concepts Based Programs

---
+++ Epam/src/main/java/org/bouquet/Flower.java
@@ -0,0 +1,20 @@
+package org.bouquet;
+
+abstract class Flower {
+    private String name;
+    private double cost;
+
+    public Flower(String name, double cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+}
---
+++ Epam/src/main/java/org/bouquet/Rose.java
@@ -0,0 +1,7 @@
+package org.bouquet;
+
+public class Rose extends Flower {
+    public Rose() {
+        super("Rose", 1);
+    }
+}
